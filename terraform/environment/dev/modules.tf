1257456c52ba67db2af6a703af54eb6bb9500a70
Alan Warnock <alan.warnock@outlook.com>
2022-07-13 12:34:01 +0100

    test pr

--- terraform/environment/dev/modules.tf
+++ terraform/environment/dev/modules.tf
@@ -5,3 +5,11 @@ module "storage_account" {
   resource_group_name  = azurerm_resource_group.resourcegroup.name
   storage_account_name = "${var.environment}${var.location}stg"
 }
+
+module "storage_account" {
+  source               = "../../../modules/storage"
+  location             = var.location
+  environment          = var.environment
+  resource_group_name  = azurerm_resource_group.resourcegroup.name
+  storage_account_name = "${var.environment}${var.location}stg2"
+}
