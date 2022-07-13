9c5c0238fb9228ae267903dbfa090c420acb47dd
Alan Warnock <alan.warnock@outlook.com>
2022-07-13 14:53:20 +0100

    small updates

--- terraform/environment/test/modules.tf
+++ terraform/environment/test/modules.tf
@@ -1,7 +1,7 @@
 module "storage_account" {
-  source                = "../../../modules/storage"
-  location              = var.location
-  environment           = var.environment
-  resource_group_name   = azurerm_resource_group.resourcegroup.name
-  storage_account_name  = "${var.environment}${var.location}stg"
+  source               = "../../../modules/storage"
+  location             = var.location
+  environment          = var.environment
+  resource_group_name  = azurerm_resource_group.resourcegroup.name
+  storage_account_name = "${var.environment}${var.location}stg"
 }
