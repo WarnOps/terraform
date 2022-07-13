d32ad4ac94ae7680d275726d7fecf733150c1317
Alan Warnock <alan.warnock@outlook.com>
2022-07-13 11:55:36 +0100

    azure creds

--- terraform/environment/dev/providers.tf
+++ terraform/environment/dev/providers.tf
@@ -12,4 +12,9 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
+  tenant_id       = var.TENANT_ID
 }
